030797d8ba7283e4359dd29d49c1ae5bcf17c381
Doudou-samb02 <doudousamb02@gmail.com>
2025-11-25 20:59:57 +0000

    Implement header style change on scroll
    Add scroll event listener to change header style

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Changer le style du header au scroll
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
 
